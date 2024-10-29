eed4a003ce4ce2b83c77ec351f5723f86aaf77e6
wratixor <wratixor@gmail.com>
2024-10-30 00:56:32 +0300

    lorem ipsum

--- web_app/views.py
+++ web_app/views.py
@@ -1,11 +1,17 @@
+import datetime
+
 import aiohttp_jinja2
+
+year: int = datetime.date.today().year
 
 @aiohttp_jinja2.template("index.html")
 async def index(request):
-    return {'title': 'Тестовая страничка'}
+    return {'title': 'Wratixor.ru - личный сайт кодера-стихоплюя',
+            'year': year}
 
 
 @aiohttp_jinja2.template("index.html")
 async def project(request):
     project_id = request.match_info.get('project', "all")
-    return {'title': project_id}
+    return {'title': project_id,
+            'year': year}
