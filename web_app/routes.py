de44d0bda0583fb4c4bb962cf752f312bb04af0f
wratixor <wratixor@gmail.com>
2024-10-30 23:13:12 +0300

    add template, favicon, disclaimer, banner 18+

--- web_app/routes.py
+++ web_app/routes.py
@@ -4,5 +4,7 @@ from web_app import views
 
 def setup_routes(app: aiohttp.web.Application):
    app.router.add_static('/static', 'static', append_version=True)
+   app.router.add_get('/admin', views.admin)
    app.router.add_get("/", views.index)
-   app.router.add_get('/{project}', views.project)
+   app.router.add_get('/{sec}', views.section)
+   app.router.add_get('/{sec}/{chapter}', views.chapter)
--- web_app/views.py
+++ web_app/views.py
@@ -9,9 +9,53 @@ async def index(request):
     return {'title': 'Wratixor.ru - личный сайт кодера-стихоплюя',
             'year': year}
 
-
 @aiohttp_jinja2.template("index.html")
-async def project(request):
-    project_id = request.match_info.get('project', "all")
-    return {'title': project_id,
+async def section(request):
+    title = request.match_info.get('sec', "all")
+    center: int = (int(request.match_info.get('sec')[-1:]) % 2)
+    return {'title': title,
+            'adult': center,
+            'year': year}
+
+@aiohttp_jinja2.template("chapter.html")
+async def chapter(request):
+    title: str = f'{request.match_info.get('sec')}: {request.match_info.get('chapter')}'
+    center: int = (int(request.match_info.get('chapter')[-1:]) % 2)
+    img: str = ''
+    if center == 0:
+        img = 'description_bot.png'
+    article: str = '''
+    <p><b>Lorem ipsum dolor sit amet lorem ipsum dolor sit amet</b></p>
+    <i>Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor</i><br/>
+    <code>Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor</code><br/>
+    <p>
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet Lorem ipsum dolor sit amet lorem ipsum dolor
+    </p>
+    '''
+    return {'title': title,
+            'adult': center,
+            'center': center,
+            'descimg': img,
+            'article': article,
+            'year': year}
+
+@aiohttp_jinja2.template("admin.html")
+async def admin(request):
+
+    return {'title': 'Админка',
             'year': year}
